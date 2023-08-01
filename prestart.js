749264a654ebbc7c89fcfd45dfea05e4590257b1
xWass <spectatorgaming02@gmail.com>
2023-07-31 20:10:51 -0400

    merge dbinit and deploy

--- prestart.js
+++ prestart.js
@@ -54,3 +54,26 @@ const rest = new REST().setToken(process.env.TOKEN);
         console.error(error);
     }
 })();
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize({
+    host: "localhost",
+    dialect: "sqlite",
+    storage: "database.sqlite",
+    logging: false,
+});
+
+const Warns = require("./models/warns")(sequelize, Sequelize.DataTypes);
+
+const force = process.argv.includes("--force") || process.argv.includes("-f");
+
+sequelize
+    .sync({ force })
+    .then(async () => {
+        const warns = await Warns.findAll();
+        console.log(warns);
+        console.log("Database synced");
+        sequelize.close();
+    })
+    .catch(console.error);
